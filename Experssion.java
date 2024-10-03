980ac154727ffda8cdc294b42718f1176cf14eb6
Shantanu Potdar <91558870+shantanu23potdar@users.noreply.github.com>
2024-10-03 11:48:33 +0530

    Add files via upload

---
+++ Experssion.java
@@ -0,0 +1,37 @@
+public class Experssion {
+    public static void main(String[] args) {
+
+//        //int x =5;
+//        //int y =5;
+//        int a = 2;
+//        int b = 5;
+//        int c = 4;
+//        //int tot = (x+y) /2;
+//        //double tot =  (Math.pow(b,2) - 4 *a*c) / 2*a;
+//
+//        int y =6;
+//        int x = ++y * 8;
+//       // int x = y++ * 8;
+//        System.out.println(x);
+
+//        float a =7.0f /4.0f;
+//        float b = 9.0f/2.0f;
+//        float c = a * b;
+//        System.out.println(c);
+
+//        int a = 2;
+//        int v = 6;
+//        int u = 4;
+//        int s = 3;
+//       double x = (Math.pow(v,2) - Math.pow(u,2)) / 2*a*s;
+//
+//        System.out.println(x);
+
+//        int x = 7;
+//        int a = 7 * (49/7) + (35/7);
+//        System.out.println(a);
+
+        System.out.println(67>3);
+        System.out.println(67<3);
+    }
+}
