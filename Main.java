d7e964695db5ca6a567643a5c74e4dfafb1821a8
Shantanu Potdar <91558870+shantanu23potdar@users.noreply.github.com>
2024-10-05 22:50:02 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Adding Three numbers: ");
+        int num1 = 4;
+        int num2 = 3;
+        int num3 = 5;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
