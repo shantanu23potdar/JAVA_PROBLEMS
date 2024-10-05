312d7203d3a2625c9ed2dc2a5890028f93e842be
Shantanu Potdar <91558870+shantanu23potdar@users.noreply.github.com>
2024-10-05 22:50:31 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,114 @@
+
+public class Pattern {
+    public static void main(String[] args) {
+
+//        pattern
+//        int n = 4;
+//         for (int i = n; i > 0; i--) {
+//            for (int j = 0; j < i; j++) {
+//                 System.out.print("*");
+//            }
+//            System.out.print("\n");
+//    }
+
+        //sum 1st n even number for loop
+//      1+2+3=6
+//        int sum = 0;
+//        int n = 3;
+//        for (int i = 1; i < n; i++ ) {
+//            sum = sum + (2 * i);
+//        }
+//            System.out.println(sum);
+
+           //sum 1st n even number while loop
+
+//        int sum= 0;
+//        int n = 3;
+//        while (n > 3) {
+//            n++;
+//        }
+//        sum = sum + (2 * n);
+//        System.out.println(sum);
+
+
+        //multiple table 5
+//        int n = 5;
+//        for (int i=1;i<=10;i++){
+//            System.out.printf("%d X %d = %d\n" ,n,i,n*i);
+//        }
+
+//        while loop
+//        int n = 5;
+//        int i= 0 ;
+//        while (i < 10){
+//            i++;
+//            System.out.printf("%d X %d = %d\n" ,n,i,n*i);
+//        }
+
+       //multiple table 10
+//        int n = 10;
+//        for (int i=10;i>=1;i--){
+//            System.out.printf("%d X %d = %d\n" ,n,i,n*i);
+//        }
+
+      //  while loop
+//        int n = 10;
+//        int i= 11 ;
+//        while (i > 1){
+//            i--;
+//            System.out.printf("%d X %d = %d\n" ,n,i,n*i);
+//        }
+
+        // Factorial 5!=5*4*3*2*1 = 120
+//        int n=5;
+//        int factorial = 1;
+//        for (int i  = 1;i <= n ;i++){
+//           factorial *= i;
+//
+//        }
+//        System.out.println(factorial);
+
+
+//      //  Factorial 5! while loop
+//        int n=5;
+//        int factorial = 1;
+//        int i=1;
+//        while(i<=n){
+//            factorial *= i;
+//            i++;
+//
+//
+//        }
+//        System.out.println(factorial);
+
+
+     //   pattern while loop
+
+//        int j;
+//        int i = 5 ;   //row-1
+//        while ( i >= 0)
+//        {
+//            j = 1 ;
+//            while ( j <= i )
+//            {
+//                System.out.print("*");
+//               j++;
+//            }
+//            System.out.println();
+//            i--;
+//        }
+
+//multiple and sum table 5
+//        int n = 1;
+//        int sum = 0 ;
+//        for (int i=1;i<=10;i++){
+//            sum = sum + (n*i);
+//
+//        }
+//        System.out.println(sum);
+
+
+    }
+
+    }
+
