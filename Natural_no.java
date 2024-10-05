963c6b654d8bee0041f92caffb5917406a291e8b
Shantanu Potdar <91558870+shantanu23potdar@users.noreply.github.com>
2024-10-05 22:50:17 +0530

    Add files via upload

---
+++ Natural_no.java
@@ -0,0 +1,19 @@
+public class Natural_no {
+    public static void main(String[] args) {
+//while loop
+//       int n = 100;
+//       while(n<=200)
+//       {
+//           System.out.println(n);
+//            n++;
+//        }
+
+        //do-while loop
+//        int n = 100;
+//        do{
+//            System.out.println(n);
+//            n++;
+//        }while(n<=200);
+
+    }
+}
