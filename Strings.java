16ae98d2dc185aa4ea1059a62fce2a50a279978d
Shantanu Potdar <91558870+shantanu23potdar@users.noreply.github.com>
2024-10-09 13:40:40 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Shantanu";
+       // System.out.println(name);
+
+        //int value =name.length();
+//        String value =name.toUpperCase();
+      //  String value =name.toLowerCase();
+        //System.out.println(value);
+
+
+//        String nonTrimmedString= "    Shantanu    ";
+//        String trimmedString = nonTrimmedString.trim();
+//        System.out.println(trimmedString);
+//        System.out.println(nonTrimmedString.trim());
+
+        //String value = name.substring(2);
+//        String value = name.substring(3,8);
+//        System.out.println(value);
+
+        System.out.println(name.replace("a","_"));
+
+
+    }
+}
